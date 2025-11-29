4672ed4a854adecd2e18646b47a88868fa726e2d
omkar-ghongade <omkarsubhashghongade21@gmail.com>
2025-11-29 17:58:53 +0530

    update manifest

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -7,20 +7,20 @@ export async function GET() {
       "name": "nadArcade",
       "version": "1",
       "iconUrl": "https://i.ibb.co/20ZM5yZB/Whats-App-Image-2025-11-29-at-12-27-46-d3bcf721.jpg",
-      "homeUrl": "https://monad-mini-app-five.vercel.app",
-      "imageUrl": "https://monad-mini-app-five.vercel.app/image.png",
+      "homeUrl": "https://monad-mini-app.netlify.app/",
+      "imageUrl": "https://monad-mini-app.netlify.app/image.png",
       "splashImageUrl": "https://i.ibb.co/WW2HbFmy/Nad-Arcade.png",
       "splashBackgroundColor": "#6200EA",
-      "webhookUrl": "https://monad-mini-app-five.vercel.app/api/webhook",
+      "webhookUrl": "https://monad-mini-app.netlify.app/api/webhook",
       "subtitle": "Nostalgic Game",
       "description": "testing",
       "primaryCategory": "games"
     },
     "accountAssociation": {
       "header": "eyJmaWQiOjExNTEyMjcsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhiYURmNzA5RDA1NUQ3M2I2RDg4NjAyMzdlMTkxNTAxM2NhYzBmNGYzIn0",
-      "payload": "eyJkb21haW4iOiJtb25hZC1taW5pLWFwcC1maXZlLnZlcmNlbC5hcHAifQ",
-      "signature": "YkUHIxlccGb+xtBr7TJmj8mooDKLoJQ87plp0KPlUPEDM5RiZuFdo6AZu5D1zjxhLuWaTvS0hnsXPokU0Dza6xs="
-    }
+      "payload": "eyJkb21haW4iOiJodHRwczovL21vbmFkLW1pbmktYXBwLm5ldGxpZnkuYXBwLyJ9",
+      "signature": "FMzSAp8XveN4X2cfnb0rkDvGr5w9FIoFVWj/4cEodbUETDMrxvMSs6UfJ5gObCpEM4W2YdRexxsE7PGcj4uHWRw="
+    } 
   };
 
   return NextResponse.json(farcasterConfig);
