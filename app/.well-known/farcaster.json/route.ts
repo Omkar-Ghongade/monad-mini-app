7c6170349b35891e58899af0b50009ae71aa8e82
omkar-ghongade <omkarsubhashghongade21@gmail.com>
2025-11-29 11:21:24 +0530

    added manifest

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,21 +3,24 @@ import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
   const farcasterConfig = {
-    // TODO: Add your own account association
-    frame: {
-      version: "1",
-      name: "Monad Farcaster MiniApp Template",
-      iconUrl: `${APP_URL}/images/icon.png`,
-      homeUrl: `${APP_URL}`,
-      imageUrl: `${APP_URL}/images/feed.png`,
-      screenshotUrls: [],
-      tags: ["monad", "farcaster", "miniapp", "template"],
-      primaryCategory: "developer-tools",
-      buttonTitle: "Launch Template",
-      splashImageUrl: `${APP_URL}/images/splash.png`,
-      splashBackgroundColor: "#ffffff",
-      webhookUrl: `${APP_URL}/api/webhook`,
+    "frame": {
+      "name": "Nokia Snake",
+      "version": "1",
+      "iconUrl": "https://monad-mini-app-five.vercel.app/icon.png",
+      "homeUrl": "https://monad-mini-app-five.vercel.app",
+      "imageUrl": "https://monad-mini-app-five.vercel.app/image.png",
+      "splashImageUrl": "https://monad-mini-app-five.vercel.app/splash.png",
+      "splashBackgroundColor": "#6200EA",
+      "webhookUrl": "https://monad-mini-app-five.vercel.app/api/webhook",
+      "subtitle": "Nostalgic Game",
+      "description": "testing",
+      "primaryCategory": "games"
     },
+    "accountAssociation": {
+      "header": "eyJmaWQiOjExNTEyMjcsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhiYURmNzA5RDA1NUQ3M2I2RDg4NjAyMzdlMTkxNTAxM2NhYzBmNGYzIn0",
+      "payload": "eyJkb21haW4iOiJtb25hZC1taW5pLWFwcC1maXZlLnZlcmNlbC5hcHAifQ",
+      "signature": "YkUHIxlccGb+xtBr7TJmj8mooDKLoJQ87plp0KPlUPEDM5RiZuFdo6AZu5D1zjxhLuWaTvS0hnsXPokU0Dza6xs="
+    }
   };
 
   return NextResponse.json(farcasterConfig);
